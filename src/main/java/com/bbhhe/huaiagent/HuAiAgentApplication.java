19972a81b2ea561484180468b21255882b6d37d6
binbin <a19936300@users.noreply.github.com>
2025-05-06 22:37:32 +0800

    初始化项目

---
+++ src/main/java/com/bbhhe/huaiagent/HuAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bbhhe.huaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbhhe/huaiagent/HuAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbhhe.huaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
