eb87043503ef24f0c19b137d4d146abcc10716b6
binbin <binbin@xiu.com>
2025-05-26 16:55:38 +0800

    笔记4-rag基础知识

--- src/main/java/com/bbhhe/huaiagent/app/LoveApp.java
+++ src/main/java/com/bbhhe/huaiagent/app/LoveApp.java
@@ -1,12 +1,16 @@
 package com.bbhhe.huaiagent.app;
 
 import com.bbhhe.huaiagent.advisor.MyLoggerAdvisor;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -57,5 +61,43 @@ public class LoveApp {
                 .stream()
                 .content();
     }
+
+//    @Resource
+//    private VectorStore vectorStore;
+//
+//    public String doChatWithRag(String message, String chatId){
+//        ChatResponse chatResponse = chatClient.prompt()
+//                .user(message)
+//                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+//                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+//                .advisors(new MyLoggerAdvisor())
+//                .advisors(new QuestionAnswerAdvisor(vectorStore))
+//                .call()
+//                .chatResponse();
+//        String text = chatResponse.getResult().getOutput().getText();
+//        //log.info("content:{}",text);
+//        return text;
+//    }
+
+    @Resource
+    private Advisor loveAppRagCloudAdvisor;
+
+    public String doChatWithRag(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                // 应用增强检索服务（云知识库服务）
+                .advisors(loveAppRagCloudAdvisor)
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        //log.info("content: {}", content);
+        return content;
+    }
+
 
 }
